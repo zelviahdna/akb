afe5698d1634682d263f6816f0f0dba015a4001a
zelvia <105841105922@student.unismuh.ac.id>
2025-07-28 13:20:17 +0800

    selesai tugas 4

--- app/index.tsx
+++ app/index.tsx
@@ -1,86 +1,102 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Image,
-  Pressable,
-  StyleSheet,
-  Dimensions,
-  ScrollView,
-} from 'react-native';
+import { useFonts } from "expo-font";
+import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from "react-native";
 
-const getImagePairs = () => {
-  const baseNIM = '10584110';
-  const suffixNIM = '22';
-  const baseURL = 'https://simak.unismuh.ac.id/upload/mahasiswa/';
-  const imgSuffix = '_.jpg?1751871539';
-  const defaultImg =
-    'https://uploads-us-west-2.insided.com/figma-en/attachment/7105e9c010b3d1f0ea893ed5ca3bd58e6cec090e.gif';
+const mahasiswaData = [
+  {
+    nama: "Alya Anandha",
+    nim: "105841105422",
+    style: { fontFamily: "Barlow_ExtraBold", fontSize: 26, color: "green", fontWeight: "bold" }
+  },
+  {
+    nama: "M.Fikri Haikal Ayatullah",
+    nim: "105841105522",
+    style: { fontFamily: "FiraSans-BlackItalic", fontSize: 24 }
+  },
+  {
+    nama: "Muh.Dirham Rahim",
+    nim: "105841105622",
+    style: { fontFamily: "IntelOneMono-VariableFont_wght", fontSize: 24 }
+  },
+  {
+    nama: "Zaskya Aulia Ashar",
+    nim: "105841105722",
+    style: { fontFamily: "LibertinusMono-Regular", fontSize: 24 }
+  },
+  {
+    nama: "Muh.Ilham Akbar",
+    nim: "105841105822",
+    style: { fontFamily: "Oswald-VariableFont_wght", fontSize: 24 }
+  },
+  {
+    nama: "ANDI DIFHTA RAMEYZA KAILAh",
+    nim: "105841106022",
+    reverse: true,
+    style: { fontFamily: "PTSans-Italic", fontSize: 24 }
+  },
+  {
+    nama: "Arsifah ainun aulia",
+    nim: "105841106122",
+    reverse: true,
+    style: { fontFamily: "RobotoCondensed-VariableFont_wght", fontSize: 24 }
+  },
+  {
+    nama: "Tegar Surya Prayoga",
+    nim: "105841106222",
+    reverse: true,
+    style: { fontFamily: "RobotoMono-Italic-VariableFont_wght", fontSize: 24 }
+  },
+  {
+    nama: "ANDI ANGKE RIONA MEGAWAN",
+    nim: "105841106322",
+    reverse: true,
+    style: { fontFamily: "SpaceMono-Regular", fontSize: 24 }
+  },
+  {
+    nama: "A. FACHRI",
+    nim: "105841106522",
+    reverse: true,
+    style: { fontFamily: "SpecialGothicExpandedOne-Regular", fontSize: 24 }
+  }
+];
 
-  return Array.from({ length: 10 }, (_, i) => {
-    const nim = `${baseNIM}${i + 59}${suffixNIM}`;
-    return {
-      mainImage: `${baseURL}${nim}${imgSuffix}`,
-      altImage: defaultImg,
-    };
-  });
-};
+export default function Index() {
+  const [fontsLoaded] = useFonts({
+  "Barlow_ExtraBold": require("../assets/fonts/Barlow-ExtraBold.ttf"),
+  "FiraSans-BlackItalic": require("../assets/fonts/FiraSans-BlackItalic.ttf"),
+  "IntelOneMono-VariableFont_wght": require("../assets/fonts/IntelOneMono-VariableFont_wght.ttf"),
+  "LibertinusMono-Regular": require("../assets/fonts/LibertinusMono-Regular.ttf"),
+  "Oswald-VariableFont_wght": require("../assets/fonts/Oswald-VariableFont_wght.ttf"),
+  "PTSans-Italic": require("../assets/fonts/PTSans-Italic.ttf"),
+  "RobotoCondensed-VariableFont_wght": require("../assets/fonts/RobotoCondensed-VariableFont_wght.ttf"),
+  "RobotoMono-Italic-VariableFont_wght": require("../assets/fonts/RobotoMono-Italic-VariableFont_wght.ttf"),
+  "SpaceMono-Regular": require("../assets/fonts/SpaceMono-Regular.ttf"),
+  "SpecialGothicExpandedOne-Regular": require("../assets/fonts/SpecialGothicExpandedOne-Regular.ttf"),
+});
 
-const imagePairs = getImagePairs();
-
-const GridGambarAgus = () => {
-  const [states, setStates] = useState(
-    imagePairs.map(() => ({ zoom: 1, showAlt: false }))
-  );
-
-  const toggleImage = (index: number) => {
-    setStates((prev) =>
-      prev.map((item, i) =>
-        i === index
-          ? {
-              zoom: item.zoom < 2 ? item.zoom * 1.2 : 2,
-              showAlt: !item.showAlt,
-            }
-          : item
-      )
-    );
-  };
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" style={{ flex: 1, justifyContent: "center", alignItems: "center" }} />;
+  }
 
   return (
-    <ScrollView contentContainerStyle={styles.grid}>
-      {imagePairs.map((pair, index) => (
-        <Pressable key={index} onPress={() => toggleImage(index)}>
-          <Image
-            source={{
-              uri: states[index].showAlt ? pair.altImage : pair.mainImage,
-            }}
-            style={[
-              styles.image,
-              { transform: [{ scale: states[index].zoom }] },
-            ]}
-          />
-        </Pressable>
+    <ScrollView contentContainerStyle={styles.container}>
+      {mahasiswaData.map((item, index) => (
+        <View key={index}>
+          <Text style={[styles.baseText, item.style]}>
+            {item.reverse
+              ? `${item.nim}\n${item.nama}`
+              : `${item.nama}\n${item.nim}`}
+          </Text>
+        </View>
       ))}
     </ScrollView>
   );
-};
-
-export default GridGambarAgus;
+}
 
 const styles = StyleSheet.create({
-  grid: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    padding: 8,
+  container: {
+    padding: 20
   },
-  image: {
-    width: Dimensions.get('window').width / 3 - 16,
-    height: Dimensions.get('window').width / 3 - 16,
-    margin: 4,
-    borderRadius: 8,
-    resizeMode: 'cover',
-    backgroundColor: '#eee',
-    borderWidth: 1,
-    borderColor: '#bbb',
-  },
+  baseText: {
+    marginVertical: 4
+  }
 });

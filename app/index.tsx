25806cfdd1777b8adb7a7eba73e5cbbdf7e0892f
zelvia <105841105922@student.unismuh.ac.id>
2025-08-04 19:18:18 +0800

    tugas 6 selesai

--- app/index.tsx
+++ app/index.tsx
@@ -1,125 +1,28 @@
-import { useFonts } from "expo-font";
-import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from "react-native";
-
-const mahasiswaData = [
-  {
-    nama: "Alya Anandha",
-    nim: "105841105422",
-    style: { fontFamily: "Barlow_ExtraBold", fontSize: 24 }
-  },
-  {
-    nama: "M.Fikri Haikal Ayatullah",
-    nim: "105841105522",
-    style: { fontFamily: "FiraSans-BlackItalic", fontSize: 24 }
-  },
-  {
-    nama: "Muh.Dirham Rahim",
-    nim: "105841105622",
-    style: { fontFamily: "LibertinusMono-Regular", fontSize: 24 }
-  },
-  {
-    nama: "Zaskya Aulia Ashar",
-    nim: "105841105722",
-    style: { fontFamily: "PTSans-Italic", fontSize: 24 }
-  },
-  {
-    nama: "Muh.Ilham Akbar",
-    nim: "105841105822",
-    style: { fontFamily: "SpecialGothicExpandedOne-Regular", fontSize: 24 }
-  },
-  {
-    nama: "ANDI DIFHTA RAMEYZA KAILAh",
-    nim: "105841106022",
-    style: { fontFamily: "IntelOneMono-VariableFont_wght", fontSize: 24 }
-  },
-  {
-    nama: "Arsifah ainun aulia",
-    nim: "105841106122",
-    style: { fontFamily: "Oswald-VariableFont_wght", fontSize: 24 }
-  },
-  {
-    nama: "Tegar Surya Prayoga",
-    nim: "105841106222",
-    style: { fontFamily: "RobotoCondensed-VariableFont_wght", fontSize: 24 }
-  },
-  {
-    nama: "ANDI ANGKE RIONA MEGAWAN",
-    nim: "105841106322",
-    style: { fontFamily: "RobotoMono-Italic-VariableFont_wght", fontSize: 24 }
-  },
-  {
-    nama: "A. FACHRI",
-    nim: "105841106522",
-    style: { fontFamily: "RobotoFlex-VariableFont", fontSize: 24 }
-  }
-];
+import React from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Index() {
-  const [fontsLoaded] = useFonts({
-    "Barlow_ExtraBold": require("../assets/fonts/Barlow-ExtraBold.ttf"),
-    "FiraSans-BlackItalic": require("../assets/fonts/FiraSans-BlackItalic.ttf"),
-    "LibertinusMono-Regular": require("../assets/fonts/LibertinusMono-Regular.ttf"),
-    "PTSans-Italic": require("../assets/fonts/PTSans-Italic.ttf"),
-    "SpecialGothicExpandedOne-Regular": require("../assets/fonts/SpecialGothicExpandedOne-Regular.ttf"),
-
-    "IntelOneMono-VariableFont_wght": require("../assets/fonts/IntelOneMono-VariableFont_wght.ttf"),
-    "Oswald-VariableFont_wght": require("../assets/fonts/Oswald-VariableFont_wght.ttf"),
-    "RobotoCondensed-VariableFont_wght": require("../assets/fonts/RobotoCondensed-VariableFont_wght.ttf"),
-    "RobotoMono-Italic-VariableFont_wght": require("../assets/fonts/RobotoMono-Italic-VariableFont_wght.ttf"),
-    "RobotoFlex-VariableFont": require("../assets/fonts/RobotoFlex-VariableFont.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return (
-      <ActivityIndicator
-        size="large"
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      />
-    );
-  }
-
-  const ascendingData = [...mahasiswaData]
-    .sort((a, b) => a.nama.localeCompare(b.nama))
-    .slice(0, 5);
-
-  const descendingData = [...mahasiswaData]
-    .sort((a, b) => b.nim.localeCompare(a.nim))
-    .slice(0, 5);
-
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.sectionTitle}>5 Nama Pertama (Ascending A-Z)</Text>
-      {ascendingData.map((item, index) => (
-        <View key={index}>
-          <Text style={[styles.baseText, item.style]}>
-            {`${item.nama}\n${item.nim}`}
-          </Text>
-        </View>
-      ))}
+    <ScrollView contentContainerStyle={{ padding: 20, flexGrow: 1 }}>
+      <Text style={{ fontSize: 20, marginBottom: 10 }}>Daftar 10 Ikon:</Text>
 
-      <Text style={styles.sectionTitle}>5 Nama Terakhir (Descending NIM)</Text>
-      {descendingData.map((item, index) => (
-        <View key={index + 100}>
-          <Text style={[styles.baseText, item.style]}>
-            {`${item.nim}\n${item.nama}`}
-          </Text>
-        </View>
-      ))}
+      <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between" }}>
+        <AntDesign name="stepforward" size={30} color="black" />
+        <AntDesign name="stepbackward" size={30} color="black" />
+        <Entypo name="500px" size={30} color="black" />
+        <Entypo name="aircraft" size={30} color="black" />
+        <EvilIcons name="bell" size={30} color="black" />
+        <EvilIcons name="calendar" size={30} color="black" />
+        <Feather name="activity" size={30} color="black" />
+        <Feather name="airplay" size={30} color="black" />
+        <FontAwesome name="heart" size={30} color="black" />
+        <FontAwesome name="star" size={30} color="black" />
+      </View>
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20
-  },
-  baseText: {
-    marginVertical: 6
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 24,
-    marginBottom: 12
-  }
-});
